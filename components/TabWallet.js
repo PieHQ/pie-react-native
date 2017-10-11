6f9e6a087cf9a64d1db34b315a18371f4d6d43ac
Kator James <kator95@gmail.com>
2017-10-11 06:21:30 +0100

    Finished Wallet Tab

--- components/TabWallet.js
+++ components/TabWallet.js
@@ -1,16 +1,6 @@
 import React, { Component } from 'react'
 import { Text, View, StyleSheet, Dimensions, TextInput } from 'react-native'
-import {
-  ActionsContainer,
-  Button,
-  FieldsContainer,
-  Fieldset,
-  Form,
-  FormGroup,
-  Input,
-  Label,
-  Switch
-} from 'react-native-clean-form'
+import { Button } from 'react-native-elements'
 
 import Colors from '../assets/literals/colors'
 
@@ -19,28 +9,8 @@ const Screen = Dimensions.get('window')
 export default class Wallet extends Component {
   constructor(props) {
     super(props)
-    this.handleWalletEnquiry = this.walletEnquiry.bind(this)
     this.state = {
-      submitDisabled: true,
-      wallets: []
-    }
-  }
-
-  walletEnquiry(e) {
-    let phone = e.target.value
-    if(phone.length >= 11) {
-      const { loading, api } = this.props
-      loading.showLoading()
-      api.post('walletEnquiry', { phone }).then(response => {
-        loading.hideLoading()
-        if(response && response.data && response.data.wallets) {
-          this.setState({ wallets: response.data.wallets, submitDisabled: false })
-        } else {
-          this.setState({ submitDisabled: true })
-        }
-      })
-    } else {
-      this.setState({ submitDisabled: true })
+      phone: ''
     }
   }
 
@@ -48,15 +18,26 @@ export default class Wallet extends Component {
     const { sentData, onSubmit } = this.props
     return(
       <View>
-        <TextInput
-          style={[styles.input]}
-          underlineColorAndroid="transparent"
-          placeholder="Phone Number"
-          placeholderTextColor="#666666"
-          selectionColor="#666666"
-          keyboardType="phone-pad"
-          maxLength={11}
-          autoCapitalize="none" />
+        <View>
+          <TextInput
+            style={[styles.input]}
+            underlineColorAndroid="transparent"
+            placeholder="PHONE NUMBER"
+            placeholderTextColor="#666666"
+            selectionColor="#666666"
+            keyboardType="phone-pad"
+            maxLength={11}
+            autoCapitalize="none"
+            onChange={(phone) => this.setState({phone})} />
+        </View>
+        <View>
+          <Button
+            borderRadius={6}
+            color={Colors.white}
+            buttonStyle={{ backgroundColor: Colors.primaryColor, marginTop: 15 }}
+            onPress={() => onSubmit(this.state)}
+            title={`Pay ${sentData.currencySign}${sentData.amount}`} />
+        </View>
       </View>
     )
   }
@@ -65,7 +46,7 @@ export default class Wallet extends Component {
 const styles = StyleSheet.create({
   input: {
     margin: 15,
-    height: 50,
+    height: 40,
     borderColor: Colors.accent,
     borderWidth: 1,
     backgroundColor: Colors.inputBackground,
